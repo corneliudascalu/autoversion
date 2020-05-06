be27894a49499b9f2668126894add3272ad2f913
Corneliu Dascălu <corneliu.dascalu@gmail.com>
2020-05-06 19:46:51 +0300

    Add gradle plugin publishing plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.3.72"
     id("java-gradle-plugin")
+    id("com.gradle.plugin-publish") version "0.11.0"
 }
 
 repositories {
@@ -11,9 +12,17 @@ gradlePlugin {
     plugins {
         create("autoversion") {
             id = "com.corneliudascalu.autoversion"
+            displayName = "AutoVersion"
+            description = "Auto versioning based on the most recent git tag"
             implementationClass = "com.corneliudascalu.AutoVersionPlugin"
         }
     }
+}
+
+pluginBundle {
+    website = "http://www.corneliudascalu.com"
+    vcsUrl = "https://github.com/corneliudascalu/autoversion"
+    tags = listOf("versioning", "git", "tag")
 }
 
 dependencies {
